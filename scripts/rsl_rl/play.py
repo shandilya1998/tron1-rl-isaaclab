84d6ea4995fa95ed894f2cfa92c7286916942d96
shandilya1998 <shreyas.shandilya@gmail.com>
2026-03-02 10:50:51 +0000

    Bug Fixes
    Problem
    =======
    1. Missing parenthesis in package `himloco`
    2. Error in picking correct algorithm and policy with HIM
    
    Solution
    ========
    1. Added missing parenthesis in
       `himloco/himloco/modules/him_actor_critic.py`
    2. Added configuration statement to ensure `HIMPPO` and `HIMActorCritic`
       are used
    
    Note
    ====

--- scripts/rsl_rl/play.py
+++ scripts/rsl_rl/play.py
@@ -266,8 +266,13 @@ def main():
     # wrap around environment for rsl-rl
     env = RslRlVecEnvWrapper(env)
     # load previously trained model
-    print(f"[INFO]: Loading model checkpoint from: {resume_path}")
+    print(
+        f"[INFO]: Loading model checkpoint from: {resume_path} for policy type {args_cli.policy_type}"
+    )
+    ppo_runner = None
     if args_cli.policy_type == "HIMPPO":
+        agent_cfg.policy.class_name = "HIMActorCritic"
+        agent_cfg.algorithm.class_name = "HIMPPO"
         ppo_runner = HIMOnPolicyRunner(
             env, agent_cfg.to_dict(), log_dir=None, device=agent_cfg.device
         )
